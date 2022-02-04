5edd13bd3d0ff1e55fcfdbfbcefad4ee1a25847b
iamkmai <45348225+iamkmai@users.noreply.github.com>
2022-02-04 19:17:38 +0900

    basic level 基本的な型

---
+++ src/typeList.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** TypeScriptの基本の型 */
+
+// boolean
+let bool: boolean = true;
+
+// number 数値
+let nom: number = 0;
+
+// string 文字
+let str: string = "A";
+
+// Array 配列
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void "特に何も返さないのであれば記述不要"
+const funcA = (): void => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let obj１: object = {}; // あんまり使う機会ないかも
+let obj２: { id: number; name: string } = {id:1, "AAA"};
